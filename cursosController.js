df624a03d4777f00633295a1db9db51c9a6134ee
carloscrovara <carloscrovara@gmail.com>
2020-11-26 20:23:50 -0300

    Version 1.0

--- cursosController.js
+++ cursosController.js
@@ -2,14 +2,82 @@
 let data = require("./cursosModel");
 
 
-
-function GetAllCursos(req, res){
+function getAllCursos(req, res){
     res.json(data);
 }
+
+function getOneCurso(req, res){
+    let curso = data.find(function (item) {
+      return item.id === parseInt(req.params.id);
+    });
+  
+    if (curso) {
+      res.status(200).json(curso);
+    } else {
+      res.status(404).json("No se encontró curso");
+    }
+  }
+  
+  function createCurso(req, res){
+    let itemIds = data.map((item) => item.id);
+    let nuevoId = itemIds.length > 0 ? Math.max.apply(Math, itemIds) + 1 : 1;
+  
+    let nuevoCurso = {
+      id: nuevoId,
+      nombreCurso: req.body.nombre,
+      duracion: req.body.precio,
+    };
+
+    data.push(nuevoCurso);
+
+    res.status(201).json(nuevoCurso);
+  }
+  
+  function modifyCurso(req, res){
+    let curso = data.find(function (item) {
+      return item.id === parseInt(req.params.id);
+    });
+  
+    if (curso) {
+      let modificarCurso = {
+        id: req.body.id,
+        nombreCurso: req.body.nombre,
+        duracion: req.body.precio,
+      };
+  
+      let encontrado = data.indexOf(curso);
+  
+      data.splice(encontrado, 1, modificarCurso);
+  
+      res.status(200).json("Curso modificado");
+    } else {
+      res.status(404).json("No existe el curso");
+    }
+  }
+  
+  function deleteCurso(req, res){
+    let curso = data.find(function (item) {
+      return item.id === parseInt(req.params.id);
+    });
+  
+    if (curso) {
+      let encontrado = data.indexOf(curso);
+  
+      data.splice(encontrado, 1);
+  
+      res.status(200).json("curso eliminado");
+    } else {
+      res.status(404).json("No existe el curso");
+    }
+  }
 
 
 //exporto controller
 
 module.exports = {
-    GetAllCursos
+    getAllCursos,
+    getOneCurso,
+    createCurso,
+    modifyCurso,
+    deleteCurso
 }
--- cursosRoutes.js
+++ cursosRoutes.js
@@ -3,7 +3,15 @@ const app = express.Router();
 
 const cursosController = require("./cursosController");
 
-app.get("/", cursosController.GetAllCursos );
+app.get("/", cursosController.getAllCursos );
+
+app.get("/cursos/:id", cursosController.getOneCurso);
+
+app.post("/cursos/nuevo", cursosController.createCurso);
+
+app.put("/cursos/modificar/:id", cursosController.modifyCurso);
+
+app.delete("/cursos/eliminar/:id", cursosController.deleteCurso);
 
 
 module.exports = app;
